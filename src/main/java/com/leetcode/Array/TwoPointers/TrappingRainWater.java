f6b8df22feebaf0cdb27c4fdceeb31a456b5fe40
tanmoy <tanmoy92@hotmail.com>
2025-11-30 16:43:36 +0530

    Trapping Rain Water solution

---
+++ src/main/java/com/leetcode/Array/TwoPointers/TrappingRainWater.java
@@ -0,0 +1,38 @@
+package com.leetcode.Array.TwoPointers;
+
+public class TrappingRainWater {
+
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int waterTrapped = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    waterTrapped += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    waterTrapped += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return waterTrapped;
+    }
+
+    public static void main(String[] args) {
+        TrappingRainWater trappingRainWater = new TrappingRainWater();
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int result = trappingRainWater.trap(height);
+        System.out.println(result);
+    }
+}
